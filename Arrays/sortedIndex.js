7327c2f62a764ed032748b603ecf541ba6e8962c
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-03-27 10:32:37 -0400

    Add sortedIndex method sample

---
+++ Arrays/sortedIndex.js
@@ -0,0 +1,16 @@
+var _ = require('underscore');
+
+var listOfProducts = [
+    {country : "Canada", fruit : "Apple", color : "red", price : 0.75},
+    {country : "Canada", fruit : "Apple", color : "green", price : 0.80},
+    {country : "USA", fruit : "Grape", color : "yellow", price : 2},
+    {country : "USA", fruit : "Apple", color : "red", price : 0.70},
+    {country : "USA", fruit : "Lemon", color : "yellow", price : 0.60},
+    {country : "USA", fruit : "Lemon", color : "green", price : 0.65},
+    {country : "France", fruit : "Grape", color : "black", price : 2.05},
+    {country : "France", fruit : "Grape", color : "green", price : 1.95},
+    {country : "France", fruit : "Apple", color : "yellow", price : 0.85}
+];
+
+var value = _.sortedIndex(listOfProducts, {country : "France", fruit : "Grape", color : "green", price : 1.90}, 'price');
+console.log(value);
