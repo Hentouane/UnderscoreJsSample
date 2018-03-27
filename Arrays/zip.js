98bd982fadb85677955155a922cb60c835e289a1
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-03-27 10:23:02 -0400

    delete useless code

--- Arrays/zip.js
+++ Arrays/zip.js
@@ -1,16 +1,4 @@
 var _ = require('underscore');
 
-var listOfProducts = [
-    {country : "Canada", fruit : "Apple", color : "red"},
-    {country : "Canada", fruit : "Apple", color : "green"},
-    {country : "USA", fruit : "Grape", color : "yellow"},
-    {country : "USA", fruit : "Apple", color : "red"},
-    {country : "USA", fruit : "Lemon", color : "yellow"},
-    {country : "USA", fruit : "Lemon", color : "green"},
-    {country : "France", fruit : "Grape", color : "black"},
-    {country : "France", fruit : "Grape", color : "green"},
-    {country : "France", fruit : "Apple", color : "yellow"}
-];
-
 var value = _.zip(["Canada","USA","France"], ["Apple", "Grape", "Lemon"]);
 console.log(value);
